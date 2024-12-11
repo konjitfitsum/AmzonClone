0ae6c98ed106f918f79083c6ec8d81dc58930d23
Fitsum <166730412+konjitfitsum@users.noreply.github.com>
2024-12-11 15:20:41 +0000

    Building orders page UI, orders query and project wrap up

--- AmzonClone/amazon-clone-june/src/Utility/reducer.js
+++ AmzonClone/amazon-clone-june/src/Utility/reducer.js
@@ -40,10 +40,15 @@ export const reducer =(state,action) => {
                 basket:newBasket
             }
 
+            case Type.EMPTY_BASKET:
+                return{
+                    ...state,
+                    basket:[],
+                }
             case Type.SET_USER:
                 return {
                     ...state,
-                    user:action.user
+                    user:action.user,
                 }
         default:
             return state;
